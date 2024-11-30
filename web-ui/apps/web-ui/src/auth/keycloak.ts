caf08d00015e104d696c84d24fa55bbabe0b6151
Aleksei Shchurov <alexiskhb@gmail.com>
2024-11-30 23:57:12 +0500

    Fix registration

--- web-ui/apps/web-ui/src/auth/keycloak.ts
+++ web-ui/apps/web-ui/src/auth/keycloak.ts
@@ -1,8 +1,8 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://localhost:8080',
   realm: 'uwatcher',
+  url: 'http://localhost/auth',
   clientId: 'web-ui',
 });
 
